03b23d9b30cb3fba825d8959dd65d0e857c43fa7
plasma018 <nthuplasma018@gmail.com>
2019-01-07 01:22:36 +0800

    setup project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBxpXBG0PPzjbkG7oPs5smkxnIkwGo0OUU",
+    authDomain: "oshop-fe5df.firebaseapp.com",
+    databaseURL: "https://oshop-fe5df.firebaseio.com",
+    projectId: "oshop-fe5df",
+    storageBucket: "",
+    messagingSenderId: "217664140603"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBxpXBG0PPzjbkG7oPs5smkxnIkwGo0OUU",
+    authDomain: "oshop-fe5df.firebaseapp.com",
+    databaseURL: "https://oshop-fe5df.firebaseio.com",
+    projectId: "oshop-fe5df",
+    storageBucket: "",
+    messagingSenderId: "217664140603"
+  }
 };
 
 /*
