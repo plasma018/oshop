c139c46d41ab35db7c6e452532bab09b2179bd68
plasma.wu <plasma.wu@104.com.tw>
2019-01-07 10:59:23 +0800

    implement logout

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe(x => {
+      console.log(x);
+    });
+  }
 
-  ngOnInit() {
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
