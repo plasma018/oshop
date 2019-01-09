9fce3210f9ab528cbb18eef6ed17a52a259bb5dd
plasma018 <nthuplasma018@gmail.com>
2019-01-10 00:07:13 +0800

    implement update product

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -12,7 +12,7 @@ import { take } from 'rxjs/operators';
 export class ProductFormComponent implements OnInit {
   categories$;
   product = {};
-
+  id;
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -20,9 +20,9 @@ export class ProductFormComponent implements OnInit {
     private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
 
-    let id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.productService.get(id).valueChanges().pipe(take(1)).subscribe(p => this.product = p)
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.productService.get(this.id).valueChanges().pipe(take(1)).subscribe(p => this.product = p)
     }
   }
 
@@ -30,7 +30,11 @@ export class ProductFormComponent implements OnInit {
   }
 
   onSubmit(product) {
-    this.productService.create(product);
+    if (this.id) {
+      this.productService.update(this.id, product);
+    } else {
+      this.productService.create(product);
+    }
     this.router.navigate(['/admin/products']);
   }
 
