7850b3b48a417235b873535bb3fdb562a4d346d2
plasma.wu <plasma.wu@104.com.tw>
2019-01-09 16:29:02 +0800

    implement products card

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from 'src/app/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -10,7 +11,7 @@ import { ProductService } from 'src/app/product.service';
 export class ProductFormComponent implements OnInit {
   categories$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(private router: Router, private categoryService: CategoryService, private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
   }
 
@@ -19,6 +20,7 @@ export class ProductFormComponent implements OnInit {
 
   onSubmit(product) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 
 }
