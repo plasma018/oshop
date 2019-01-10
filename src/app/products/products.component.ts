3cd9dfe2d72603c99ff30eb4d71fbf2684dbe71e
plasma018 <nthuplasma018@gmail.com>
2019-01-10 21:27:50 +0800

    implement homecomponent

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products$;
   categories$;
 
-  constructor() {
+  constructor(productService: ProductService, categories: CategoryService) {
+    this.products$ = productService.getAll();
+    this.categories$ = categories.getCategories();
   }
 
-  ngOnInit() {
-  }
 
 }
