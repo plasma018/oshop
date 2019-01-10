f7bcb015dfaf50c4c949a709ebca2b7f9cef4ec5
plasma.wu <plasma.wu@104.com.tw>
2019-01-10 16:53:49 +0800

    change return url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,15 @@ import { UserService } from './user.service';
 export class AppComponent {
   constructor(private userService: UserService, private auth: AuthService, router: Router) {
     auth.user$.subscribe(user => {
-      if (user) {
-        userService.save(user);
-        console.log(localStorage.getItem('returnUrl'))
-        const returnUrl = localStorage.getItem('returnUrl');
-        router.navigateByUrl(returnUrl);
-      }
+      if (!user) { return; }
+
+      userService.save(user);
+      const returnUrl = localStorage.getItem('returnUrl');
+
+      if (!returnUrl) { return; }
+
+      localStorage.removeItem('returnUrl');
+      router.navigateByUrl(returnUrl);
     });
   }
 }
