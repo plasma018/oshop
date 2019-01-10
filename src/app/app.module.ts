3fa1132e47680d0a2fbcceff801b9798d11f74bb
plasma018 <nthuplasma018@gmail.com>
2019-01-10 23:31:47 +0800

    implement product-filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { AdminAuthGuardService } from './admin-auth-guard.service';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
 import { CategoryService } from './category.service';
 import { ProductService } from './product.service';
+import { ProductFilterComponent } from './products/product-filter/product-filter.component';
 
 
 
@@ -43,7 +44,8 @@ import { ProductService } from './product.service';
     AdminProductsComponent,
     AdminOrdersComponent,
     LoginComponent,
-    ProductFormComponent
+    ProductFormComponent,
+    ProductFilterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProductService } from '../product.service';
-import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -8,12 +10,24 @@ import { CategoryService } from '../category.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
-  categories$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  category: string;
 
-  constructor(productService: ProductService, categories: CategoryService) {
-    this.products$ = productService.getAll();
-    this.categories$ = categories.getCategories();
+  constructor(route: ActivatedRoute, productService: ProductService) {
+
+    productService.getAll().pipe(
+      switchMap(p => {
+        this.products = p
+        return route.queryParamMap
+      })
+    ).subscribe(params => {
+      this.category = params.get('category');
+
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category === this.category.toLowerCase())
+        : this.products;
+    })
   }
 
 
